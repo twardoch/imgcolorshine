fb2dc7755c83c5b3a5d8ca4a8ce575f4f439835a
Adam Twardoch <adam+github@twardoch.com>
2025-06-15 00:01:08 +0200

    v1.0.0

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-python -m uzpy run -e .
+rm -rf dist/imgcolorshine*.*
+uv build
+
+python -m uzpy run -e src
 fd -e py -x autoflake -i {}
 fd -e py -x pyupgrade --py311-plus {}
 fd -e py -x ruff check --output-format=github --fix --unsafe-fixes {}
 fd -e py -x ruff format --respect-gitignore --target-version py311 {}
 repomix -i varia,.specstory,AGENT.md,CLAUDE.md,PLAN.md,SPEC.md,llms.txt,.cursorrules -o llms.txt .
-# python -m pytest
+python -m pytest
